6732ee07d86597da2bee4102160677d614902bee
DV.Sang <vansangIT2k3@gmai.com>
2023-09-28 09:22:25 +0700

    create way 2 project 1

--- Project Async/PJ1-Random Joke Generator/pj1_script.js
+++ Project Async/PJ1-Random Joke Generator/pj1_script.js
@@ -4,18 +4,37 @@ const btn = document.querySelector("#btn");
 const url =
     "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
 
-let getJoke = () => {
+let getJoke = async () => {
     jokeContainer.classList.remove("fade");
-    fetch(url)
-        .then((data) => {
-            return data.json();
-        })
-        .then((item) => {
-            jokeContainer.innerHTML = `${item.joke}`;
-            jokeContainer.classList.add("fade");
-            console.log(item.joke);
-        });
-};
+    // ! Way 1 :
+    // fetch(url)
+    //     .then((data) => {
+    //         return data.json();
+    //     })
+    //     .then((item) => {
+    //         jokeContainer.innerHTML = `${item.joke}`;
+    //         jokeContainer.classList.add("fade");
+    //         console.log(item.joke);
+    //     });
+    // ! Way 2 :
+    /**
+     *1.  Khi bạn sử dụng data.json(), bạn cần sử dụng await để chờ đợi cho
+        đến khi promise hoàn thành và trả về dữ liệu JSON. Nó sẽ trả về một
+        promise nên bạn cần await nó.
 
+      2. Để thêm hiệu ứng "fade" cho jokeContainer, bạn cần sử dụng 
+      setTimeout để chờ một chút thời gian sau khi nhận được dữ liệu 
+      trước khi thêm lớp "fade" trở lại. 
+      Bạn cũng cần sử dụng classList.add để thêm lớp "fade".
+     */
+    const response = await fetch(url);
+    const data = await response.json();
+    jokeContainer.innerHTML = `${data.joke}`;
+    jokeContainer.classList.add("fade");
+};
+/**
+Chú ý rằng tôi đã sử dụng data.joke để lấy nội dung của câu chuyện cười từ JSON trả về và đặt nó vào jokeContainer. Sau đó, tôi thêm và xóa lớp "fade" 
+để tạo hiệu ứng fade in và fade out sau khi nhận được dữ liệu.
+*/
 btn.addEventListener("click", getJoke);
 getJoke();
